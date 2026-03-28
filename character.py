292c5306accd734f8ff942415967179925b95e6c
pravd <pravdin920@gmail.com>
2026-03-28 15:31:36 +0700

    Добавлены race и character_class в Character Добавлены атрибуты race и character_class с тестами Статус тестов: Все прошли (2 passed, 0 failed)

--- character.py
+++ character.py
@@ -1,3 +1,5 @@
 class Character:
-    def __init__(self, name):
+    def __init__(self, name, race=None, character_class=None):
         self.name = name
+        self.race = race
+        self.character_class = character_class
--- test_character.py
+++ test_character.py
@@ -8,6 +8,12 @@ class TestCharacter(unittest.TestCase):
         char = Character(name="Aragorn")
         self.assertEqual(char.name, "Aragorn")
 
+    def test_character_has_race_and_class(self):
+        from character import Character
+        char = Character(name="Legolas", race="Elf", character_class="Rogue")
+        self.assertEqual(char.race, "Elf")
+        self.assertEqual(char.character_class, "Rogue")
+
 
 if __name__ == "__main__":
     unittest.main()
